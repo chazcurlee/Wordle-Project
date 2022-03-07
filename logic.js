aed8cdf94c3ca9a686f73a65dafda118dbb82f56
chazcurlee <chaz.curlee@gmail.com>
2022-03-07 18:20:57 -0500

    added stop to end of line and enter check

--- logic.js
+++ logic.js
@@ -1,11 +1,14 @@
 // Global variables
 const inputIdArr = ["sq1In", "sq2In", "sq3In", "sq4In", "sq5In", "sq6In", "sq7In", "sq8In", "sq9In", "sq10In", "sq11In", "sq12In", "sq13In", "sq14In", "sq15In", "sq16In", "sq17In", "sq18In", "sq19In", "sq20In", "sq21In", "sq22In", "sq23In", "sq24In", "sq25In", "sq26In", "sq27In", "sq28In", "sq29In", "sq30In"]
 
-const line1 = ["sq1In", "sq2In", "sq3In", "sq4In", "sq5In", "sq6In"];
-const line2 = ["sq7In", "sq8In", "sq9In", "sq10In", "sq11In", "sq12In"];
-const line3 = ["sq13In", "sq14In", "sq15In", "sq16In", "sq17In", "sq18In"];
-const line4 = ["sq19In", "sq20In", "sq21In", "sq22In", "sq23In", "sq24In"];
-const line5 = ["sq25In", "sq26In", "sq27In", "sq28In", "sq29In", "sq30In"];
+const line1 = ["sq1In", "sq2In", "sq3In", "sq4In", "sq5In"];
+const line2 = ["sq6In", "sq7In", "sq8In", "sq9In", "sq10In"];
+const line3 = ["sq11In", "sq12In", "sq13In", "sq14In", "sq15In"];
+const line4 = ["sq16In", "sq17In", "sq18In", "sq19In", "sq20In"];
+const line5 = ["sq21In", "sq22In", "sq23In", "sq24In", "sq25In"];
+const line6 = ["sq26In", "sq27In", "sq28In", "sq29In", "sq30In"];
+const fives = ["sq5In", "sq10In", "sq15In", "sq20In", "sq25In", "sq30In"];
+let actId = document.activeElement.id;
 
 
 // Classes
@@ -36,16 +39,43 @@ class Textfield {
 
 // Functions
 
-// only alpha credit: https://www.includehelp.com/code-snippets/allow-only-alphabets-in-input-field-in-javascript.aspx
-
-
-// const triMove = (oldId, newId) => {
 
 
 
 
-// }
+const enterCheck = () => {
 
+    let pos = 0;
+    let newId = null;
+    let kyCde = event.keyCode;
+    let enteredKey = document.getElementById(elId).value;
+    let capKey = enteredKey.toUpperCase();
+    let innerText = document.getElementById(elId).value;
+
+    inputIdArr.forEach((val, idx) => {
+
+    if (val === elId) {
+        pos = idx + 1;
+        
+    }
+
+    inputIdArr.forEach((val, idx) => {
+
+        if (idx === pos) {
+            newId = val;
+        }
+
+    })
+    })
+
+    document.getElementById(elId).value = innerText.replace(enteredKey, capKey);
+    console.log(newId);
+    document.getElementById(newId).toggleAttribute("disabled");
+    document.getElementById(newId).focus();
+    document.getElementById(elId).blur();
+    document.getElementById(elId).toggleAttribute("disabled");
+
+}
 
 
 
@@ -57,6 +87,7 @@ const setFocus = () => {
 
 const autoMoveCursor = (elId) => {
 
+    
     let pos = 0;
     let newId = null;
     let kyCde = event.keyCode;
@@ -68,43 +99,83 @@ const autoMoveCursor = (elId) => {
 
 
 
-// credit: https://stackoverflow.com/questions/56502735/how-do-i-make-onkeyup-only-be-an-alphabet-key
-// user: jake
-
     if (kyCde >= 65 && kyCde <= 90){
+        let actId = document.activeElement.id;
+        console.log(actId)
 
-        inputIdArr.forEach((val, idx) => {
-            
+        if (fives.includes(actId)) {
+
+
+
+        }else {
+
+            inputIdArr.forEach((val, idx) => {
             
 
-            if (val === elId) {
-                pos = idx + 1;
+                switch (actId) {
+    
+                    case "sq5In":
+                        break;
+    
+                    case "sq10In":
+                        break;
+    
+                    case "sq15In":
+                        break;
+    
+                    case "sq20In":
+                        break;
+    
+                    case "sq25In":
+                        break;
+    
+                    case "sq30In":
+                        break;
+    
+                    default:
+    
+                        if (val === elId) {
+                            pos = idx + 1;
+                            
+                        }
+    
+                        inputIdArr.forEach((val, idx) => {
+    
+                            if (idx === pos) {
+                                newId = val;
+                            }
                 
-            }
+                        })
+                        break;
+    
+                        
+                        
+    
+                }
+                
+    
+                
+    
+    
+            }) 
+    
+            document.getElementById(elId).value = innerText.replace(enteredKey, capKey);
+            console.log(newId);
+            document.getElementById(newId).toggleAttribute("disabled");
+            document.getElementById(newId).focus();
+            document.getElementById(elId).blur();
+            document.getElementById(elId).toggleAttribute("disabled");
 
 
-        })
-        
-        inputIdArr.forEach((val, idx) => {
+        }
 
-            if (idx === pos) {
-                newId = val;
-            }
-
-        })
-        
-        
-        document.getElementById(elId).value = innerText.replace(enteredKey, capKey);
-        console.log(innerText);
-        document.getElementById(newId).toggleAttribute("disabled");
-        document.getElementById(newId).focus();
-        document.getElementById(elId).blur();
-        document.getElementById(elId).toggleAttribute("disabled");
-        
         
 
     }
+
+
 }
+
 
 
 
@@ -112,11 +183,12 @@ const autoMoveCursor = (elId) => {
 // Event Listeners
 
 
-document.addEventListener("keydown", function() {
+document.addEventListener("keyup", function() {
 
     if (event.key === "Backspace") {
 
         let actId = document.activeElement.id;
+        console.log(actId)
         let pastId = null;
         
 
@@ -130,11 +202,33 @@ document.addEventListener("keydown", function() {
 
                 }else {
 
-                    pastId = idx - 1;
-                    document.getElementById(inputIdArr[pastId]).toggleAttribute("disabled");
-                    document.getElementById(inputIdArr[pastId]).focus();
-                    document.getElementById(actId).blur();
-                    document.getElementById(actId).toggleAttribute("disabled");
+                    switch (actId) {
+
+                        case "sq6In":
+                            break;
+                        
+                        case "sq11In":
+                            break;
+
+                        case "sq16In":
+                            break;
+
+                        case "sq21In":
+                            break;
+
+                        case "sq26In":
+                            break;
+
+                        default:
+
+                            pastId = idx - 1;
+                            document.getElementById(inputIdArr[pastId]).toggleAttribute("disabled");
+                            document.getElementById(inputIdArr[pastId]).focus();
+                            document.getElementById(actId).blur();
+                            document.getElementById(actId).toggleAttribute("disabled");
+                            break;
+
+                    }
 
                 }
 
@@ -155,7 +249,53 @@ document.addEventListener("keydown", function() {
 })
 
 
+document.addEventListener("keyup", function() {
 
 
+    let actId = document.activeElement.id;
+    console.log(actId)
+    let kyCde = event.keyCode;
 
-// triangle credit: https://www.w3schools.com/howto/howto_css_shapes.asp
+    if (kyCde === 13) {
+
+        switch(actId) {
+            case "sq5In":
+                console.log("enter sq5 switch success")
+                break;
+    
+            case "sq10In":
+                break;
+    
+            case "sq15In":
+                break;
+    
+            case "sq20In":
+                break;
+    
+            case "sq25In":
+                break;
+    
+            case "sq30In":
+                break;
+    
+            default:
+                console.log("enter switch default")
+                break;
+                
+    
+    
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+
+    }
+
+})
+
+
